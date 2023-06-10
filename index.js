04364fe78e2c7fe129a6b31cd819e4aa39ff6c47
Rinna Sato <rinna.sato@nightlamp.co.jp>
2023-06-10 18:07:14 +0900

    :ambulance: quickfix express static

---
+++ index.js
@@ -0,0 +1,10 @@
+// import express from 'express';
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist'));
+
+app.listen(5000, () => {
+  console.log('express start!');
+});
